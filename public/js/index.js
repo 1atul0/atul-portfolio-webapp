4d4dc361674b24a13a8fef10b8f18675e9b3173a
1atul0 <919atul@gmail.com>
2023-04-16 23:16:48 +0530

    gulp added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+//for nav bar element change color stable
+
+
+
+
+
+
+
 //  for animated text in home page
 const text = document.querySelector(".sec-text");
 
@@ -18,15 +26,15 @@ textLoad();
 setInterval(textLoad, 12000);
 
 // document.querySelector("body").style.color = "blue";
-$("#skill-development").css("background", "red");
+$("#skill-development").css("background", "#83fae2");
 // if competitive programming button clicked in skill page then work
 $("#skill-cp").click(function () {
   $(".skill-development-content").addClass("skill-invisible");
   $(".skill-coursework-content").addClass("skill-invisible");
   $(".skill-cp-content").removeClass("skill-invisible");
-  $(this).css("background", "red");
-  $("#skill-development").css("background", "#83fae2");
-  $("#skill-coursework").css("background", "#83fae2");
+  $(this).css("background", "#83fae2");
+  $("#skill-development").css("background", "#D3D3D3");
+  $("#skill-coursework").css("background", "#D3D3D3");
 });
 
 // if development button clicked in skill page then work
@@ -34,9 +42,9 @@ $("#skill-development").click(function () {
   $(".skill-cp-content").addClass("skill-invisible");
   $(".skill-coursework-content").addClass("skill-invisible");
   $(".skill-development-content").removeClass("skill-invisible");
-  $(this).css("background", "red");
-  $("#skill-cp").css("background", "#83fae2");
-  $("#skill-coursework").css("background", "#83fae2");
+  $(this).css("background", "#83fae2");
+  $("#skill-cp").css("background", "#D3D3D3");
+  $("#skill-coursework").css("background", "#D3D3D3");
 });
 
 // if coursework button clicked in skill page then work
@@ -44,7 +52,7 @@ $("#skill-coursework").click(function () {
   $(".skill-cp-content").addClass("skill-invisible");
   $(".skill-development-content").addClass("skill-invisible");
   $(".skill-coursework-content").removeClass("skill-invisible");
-  $(this).css("background", "red");
-  $("#skill-development").css("background", "#83fae2");
-  $("#skill-cp").css("background", "#83fae2");
+  $(this).css("background", "#83fae2");
+  $("#skill-development").css("background", "#D3D3D3");
+  $("#skill-cp").css("background", "#D3D3D3");
 });
